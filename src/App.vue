fe36715cfc151347f41aadc0b0060fbd168cf6f8
GTao-09 <gongtao1997@gmail.com>
2019-04-01 20:14:16 +0800

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <AppHeader></AppHeader>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from './pages/Header'
+export default {
+  name: 'App',
+  components: {
+    AppHeader: Header
+  }
+}
+</script>
+
 <style lang="stylus">
 
 </style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from './pages/Home.vue'
+import Menu from './pages/Menu.vue'
+import Management from './pages/Management.vue'
+import About from './pages/About/About.vue'
+import Landing from './pages/Landing.vue'
+import Registered from './pages/Registered.vue'
 
 Vue.use(Router)
 
@@ -11,8 +16,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'HomeLink',
       component: Home
+    },
+    {
+      path: '/menu',
+      name: 'MenuLink',
+      component: Menu
+    },
+    {
+      path: '/management',
+      name: 'ManagementLink',
+      component: Management
+    },
+    {
+      path: '/about',
+      name: 'AboutLink',
+      component: About
+    },
+    {
+      path: '/landing',
+      name: 'LandingLink',
+      component: Landing
+    },
+    {
+      path: '/registered',
+      name: 'RegisteredLink',
+      component: Registered
     },
     {
       path: '*',
